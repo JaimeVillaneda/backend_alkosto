d49a1972d8c99efcebe55c2441ce3ce475754508
Rolapso <jaimevillaneda9@gmail.com>
2025-11-10 13:47:05 -0500

    correcion de rutas

--- routes/api.php
+++ routes/api.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\Auth\RegisterController;
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\ProductController;
+use App\Http\Controllers\CartController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
